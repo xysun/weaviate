cf07cdf58727597eb6ddd52241ebc43d065bb155
Bob van Luijt <bob@semi.technology>
2020-06-12 15:55:01 +0200

    gh-1141: update path calculation

--- usecases/traverser/explorer_semantic_path.go
+++ usecases/traverser/explorer_semantic_path.go
@@ -3,159 +3,164 @@ package traverser
 import (
 	"context"
 	"fmt"
-	"time"
+	// "time"
 )
 
 const stepSize = 0.05
 
 // IS ANSWER TEST
-func (e *Explorer) semanticPath(source, target []float32) error {
-
-	fmt.Println("start in MS", time.Now().UnixNano() / int64(time.Millisecond))
-
-	// Threshold
-	threshold := float32(0.3)
-
-	// Query and Target search for gulf of mexico
-	// queryStringArr := []string{"Which river meets the Gulf of Mexico?"}
-	// solutionSentenceArray := []string{
-	// 	"Thousands of miles of levees, flood walls, and revetments have been erected to manage the Mississippi.", 
-	// 	"Since the city wasn’t going to move to suit the river, the river would be forced to stay put.", 
-	// 	"Southeast Louisiana, where the Mississippi River meets the Gulf of Mexico, as it is often depicted on maps.", 
-	// 	"Directly next door to the Center for River Studies sits the headquarters of Louisiana’s Coastal Protection and Restoration Authority.", 
-	// 	"The Corps plugged the Mississippi River Gulf Outlet with a nine-hundred-and-fifty-foot-wide rock dam and installed massive gates and pumps between the drainage canals and Lake Pontchartrain."}
-
-	// Query which is more generic to show where it found value
-	queryStringArr := []string{"Is herbalife a pyramid scheme?"}
-	solutionSentenceArray := []string{
-		"Herbalife Nutrition is a global multi-level marketing corporation that develops and sells dietary supplements.",
-		"The company was founded by Mark Hughes in 1980, and it employs an estimated 8,900 people worldwide.",
-		"The business is incorporated in the Cayman Islands, with its corporate headquarters located in Los Angeles, California.",
-		"The company operates in 94 countries through a network of approximately 4.5 million independent distributors and members.",
-		"The company has been criticized by, among others, hedge fund manager Bill Ackman of Pershing Square Capital, who claimed that Herbalife operates a 'sophisticated pyramid scheme' after taking a $1 billion short position in Herbalife stock.",
-		"Herbalife agreed to 'fundamentally restructure' its business and pay a $200 million fine as part of a 2016 settlement with the U.S. Federal Trade Commission (FTC) following accusations of it being a pyramid scheme.",
-		"In November 2017, Ackman's hedge fund closed out its short position in Herbalife.",
-		"In February 1980, Mark Hughes began selling the original Herbalife weight management product from the trunk of his car.",
-		"Hughes often stated that the genesis of his product and program stemmed from the weight loss concerns of his mother Joanne, whose premature death he attributed to an eating disorder and an unhealthy approach to weight loss.",
-		"According to one Herbalife website, the company's goal was to change the nutritional habits of the world.",
-		"His first product was a protein shake designed to help people manage their weight.",
-		"He structured his company using a direct-selling, multi-level marketing model.",
-		"In 1982, Herbalife received complaints from the Food and Drug Administration for claims made about certain products and the inclusion of mandrake, poke root, and 'food grade' linseed oil in another.",
-		"As a result of the complaints, the company modified its product claims and reformulated the product."}
-
-	// Wrong on purpose
-	// queryStringArr := []string{"Do birds fart?"}
-	// solutionSentenceArray := []string{
-	// 	"Thousands of miles of levees, flood walls, and revetments have been erected to manage the Mississippi.", 
-	// 	"Since the city wasn’t going to move to suit the river, the river would be forced to stay put.", 
-	// 	"Southeast Louisiana, where the Mississippi River meets the Gulf of Mexico, as it is often depicted on maps.", 
-	// 	"Directly next door to the Center for River Studies sits the headquarters of Louisiana’s Coastal Protection and Restoration Authority.", 
-	// 	"The Corps plugged the Mississippi River Gulf Outlet with a nine-hundred-and-fifty-foot-wide rock dam and installed massive gates and pumps between the drainage canals and Lake Pontchartrain."}
-
-	// // overwrite source and target
-	// queryVector, err := e.vectorizer.Corpi(context.TODO(), queryStringArr)
-	// if err != nil {
-	// 	return err
-	// }
-
-	winner := "NO ANSWER FOUND... "
-	winnerDist := float32(100.0)
-
-	for _, singleSentence := range solutionSentenceArray {
-		sentence := []string{singleSentence}
-		vectorSentence, _ := e.vectorizer.Corpi(context.TODO(), sentence)
-		distance, _ := e.distancer(queryVector, vectorSentence)
-		if distance < winnerDist && distance < threshold {
-			winner = singleSentence
-			winnerDist = distance
-		}
-	}
-
-	fmt.Println("============================================================")
-	fmt.Printf("\033[1;36m%s\033[0m", "FULL CORPUS: ")
-	fmt.Println(solutionSentenceArray)
-	fmt.Printf("\033[1;36m%s\033[0m", "EXPLORE QUERY: ")
-	fmt.Println(queryStringArr)
-	fmt.Printf("\033[1;36m%s\033[0m", "WINNER SENTENCE: ")
-	fmt.Println(winner, winnerDist)
-	fmt.Println("============================================================")
-
-	fmt.Println("end in MS", time.Now().UnixNano() / int64(time.Millisecond))
-
-	return nil
-}
-
-// IS SEMANTIC PATH TEST
 // func (e *Explorer) semanticPath(source, target []float32) error {
-// 	// Yoko Ono was the wife of Lennon
-// 	sourceStringArr := []string{"beatles"}
-// 	targetStringArr := []string{"yoko", "ono"}
 
-// 	// Unknown relation
-// 	// sourceStringArr := []string{"beatles"}
-// 	// targetStringArr := []string{"iphone"}
+// 	fmt.Println("start in MS", time.Now().UnixNano() / int64(time.Millisecond))
 
-// 	// // New York purchased the rights of NYC from Holland
-// 	// sourceStringArr := []string{"new", "york", "city"}
-// 	// targetStringArr := []string{"holland"}
+// 	// Threshold
+// 	threshold := float32(0.3)
 
-// 	// // Fashion designer to fashion magazine
-// 	// sourceStringArr := []string{"alexander", "mcqueen"}
-// 	// targetStringArr := []string{"vogue"}
+// 	// Query and Target search for gulf of mexico
+// 	// queryStringArr := []string{"Which river meets the Gulf of Mexico?"}
+// 	// solutionSentenceArray := []string{
+// 	// 	"Thousands of miles of levees, flood walls, and revetments have been erected to manage the Mississippi.", 
+// 	// 	"Since the city wasn’t going to move to suit the river, the river would be forced to stay put.", 
+// 	// 	"Southeast Louisiana, where the Mississippi River meets the Gulf of Mexico, as it is often depicted on maps.", 
+// 	// 	"Directly next door to the Center for River Studies sits the headquarters of Louisiana’s Coastal Protection and Restoration Authority.", 
+// 	// 	"The Corps plugged the Mississippi River Gulf Outlet with a nine-hundred-and-fifty-foot-wide rock dam and installed massive gates and pumps between the drainage canals and Lake Pontchartrain."}
+
+// 	// Query which is more generic to show where it found value
+// 	queryStringArr := []string{"Is herbalife a pyramid scheme?"}
+// 	solutionSentenceArray := []string{
+// 		"Herbalife Nutrition is a global multi-level marketing corporation that develops and sells dietary supplements.",
+// 		"The company was founded by Mark Hughes in 1980, and it employs an estimated 8,900 people worldwide.",
+// 		"The business is incorporated in the Cayman Islands, with its corporate headquarters located in Los Angeles, California.",
+// 		"The company operates in 94 countries through a network of approximately 4.5 million independent distributors and members.",
+// 		"The company has been criticized by, among others, hedge fund manager Bill Ackman of Pershing Square Capital, who claimed that Herbalife operates a 'sophisticated pyramid scheme' after taking a $1 billion short position in Herbalife stock.",
+// 		"Herbalife agreed to 'fundamentally restructure' its business and pay a $200 million fine as part of a 2016 settlement with the U.S. Federal Trade Commission (FTC) following accusations of it being a pyramid scheme.",
+// 		"In November 2017, Ackman's hedge fund closed out its short position in Herbalife.",
+// 		"In February 1980, Mark Hughes began selling the original Herbalife weight management product from the trunk of his car.",
+// 		"Hughes often stated that the genesis of his product and program stemmed from the weight loss concerns of his mother Joanne, whose premature death he attributed to an eating disorder and an unhealthy approach to weight loss.",
+// 		"According to one Herbalife website, the company's goal was to change the nutritional habits of the world.",
+// 		"His first product was a protein shake designed to help people manage their weight.",
+// 		"He structured his company using a direct-selling, multi-level marketing model.",
+// 		"In 1982, Herbalife received complaints from the Food and Drug Administration for claims made about certain products and the inclusion of mandrake, poke root, and 'food grade' linseed oil in another.",
+// 		"As a result of the complaints, the company modified its product claims and reformulated the product."}
+
+// 	// Wrong on purpose
+// 	// queryStringArr := []string{"Do birds fart?"}
+// 	// solutionSentenceArray := []string{
+// 	// 	"Thousands of miles of levees, flood walls, and revetments have been erected to manage the Mississippi.", 
+// 	// 	"Since the city wasn’t going to move to suit the river, the river would be forced to stay put.", 
+// 	// 	"Southeast Louisiana, where the Mississippi River meets the Gulf of Mexico, as it is often depicted on maps.", 
+// 	// 	"Directly next door to the Center for River Studies sits the headquarters of Louisiana’s Coastal Protection and Restoration Authority.", 
+// 	// 	"The Corps plugged the Mississippi River Gulf Outlet with a nine-hundred-and-fifty-foot-wide rock dam and installed massive gates and pumps between the drainage canals and Lake Pontchartrain."}
 
 // 	// overwrite source and target
-// 	source, err := e.vectorizer.Corpi(context.TODO(), sourceStringArr)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	target, err = e.vectorizer.Corpi(context.TODO(), targetStringArr)
+// 	queryVector, err := e.vectorizer.Corpi(context.TODO(), queryStringArr)
 // 	if err != nil {
 // 		return err
 // 	}
 
-// 	sourceWords, _, err := e.vectorizer.NearestWordsByVector(context.TODO(), source, 2500)
-// 	if err != nil {
-// 		return err
-// 	}
+// 	winner := "NO ANSWER FOUND... "
+// 	winnerDist := float32(100.0)
 
-// 	targetWords, _, err := e.vectorizer.NearestWordsByVector(context.TODO(), target, 2500)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	// Moving away from the source as start point, towards the target
-// 	lowestDistance := float32(9999.0)
-// 	for i := range sourceWords {
-// 		if string(sourceWords[i][0]) != "$" {
-// 			sourceWordVector, err := e.vectorizer.Corpi(context.TODO(), []string{string(sourceWords[i])})
-// 			if err != nil {
-// 				return err
-// 			}
-// 			distToResult, _ := e.distancer(sourceWordVector, target)
-// 			if distToResult < lowestDistance {
-// 				fmt.Println("concept:", sourceWords[i], "distanceToQuery:", distToResult, "distanceToParent:", "UNSET", "distanceToNext:", "UNSET")
-// 				lowestDistance = distToResult
-// 			}
+// 	for _, singleSentence := range solutionSentenceArray {
+// 		sentence := []string{singleSentence}
+// 		vectorSentence, _ := e.vectorizer.Corpi(context.TODO(), sentence)
+// 		distance, _ := e.distancer(queryVector, vectorSentence)
+// 		if distance < winnerDist && distance < threshold {
+// 			winner = singleSentence
+// 			winnerDist = distance
 // 		}
 // 	}
 
-// 	// Moving towards the target, furthers away from the target
-// 	for i := len(targetWords)-1; i >= 0; i-- {
-// 		if string(targetWords[i][0]) != "$" {
-// 			targetWordVector, err := e.vectorizer.Corpi(context.TODO(), []string{string(targetWords[i])})
-// 			if err != nil {
-// 				return err
-// 			}
-// 			distToResult, _ := e.distancer(targetWordVector, source)
-// 			if distToResult < lowestDistance {
-// 				fmt.Println("concept:", targetWords[i], "distanceToQuery", distToResult, "distanceToParent:", "UNSET", "distanceToNext:", "UNSET")
-// 				lowestDistance = distToResult
-// 			}
-// 		}
-// 	}
+// 	fmt.Println("============================================================")
+// 	fmt.Printf("\033[1;36m%s\033[0m", "FULL CORPUS: ")
+// 	fmt.Println(solutionSentenceArray)
+// 	fmt.Printf("\033[1;36m%s\033[0m", "EXPLORE QUERY: ")
+// 	fmt.Println(queryStringArr)
+// 	fmt.Printf("\033[1;36m%s\033[0m", "WINNER SENTENCE: ")
+// 	fmt.Println(winner, winnerDist)
+// 	fmt.Println("============================================================")
+
+// 	fmt.Println("end in MS", time.Now().UnixNano() / int64(time.Millisecond))
 
 // 	return nil
 // }
+
+// IS SEMANTIC PATH TEST
+func (e *Explorer) semanticPath(source, target []float32) error {
+	// Yoko Ono was the wife of Lennon
+	// sourceStringArr := []string{"beatles"} // the "explore question"
+	// targetStringArr := []string{"yoko", "ono"} // the "result"
+
+	// Text and query demo dataset
+	sourceStringArr := []string{"asian"}
+	targetStringArr := []string{"In South Korea, after a decade of success in light industry, officials promoted heavier industries, such as shipbuilding and chemicals. In South Korea it is more like 45%; in Taiwan, 65%; and in Singapore and Hong Kong, closer to 200%. On the most recent “trade day”, President Moon Jae-in of South Korea applauded new industries such as electrical vehicles and robots. A political dispute between South Korea and Japan, rooted in Japan’s occupation of South Korea in the first half of the 20th century, has morphed into a 21st-century trade squall. Japan has restricted sales to South Korea of materials vital for making semiconductor chips."}
+
+	// Unknown relation
+	// sourceStringArr := []string{"beatles"}
+	// targetStringArr := []string{"steve", "jobs"}
+
+	// // New York purchased the rights of NYC from Holland
+	// sourceStringArr := []string{"new", "york", "city"}
+	// targetStringArr := []string{"holland"}
+
+	// // Fashion designer to fashion magazine
+	// sourceStringArr := []string{"alexander", "mcqueen"}
+	// targetStringArr := []string{"vogue"}
+
+	// overwrite source and target
+	source, err := e.vectorizer.Corpi(context.TODO(), sourceStringArr)
+	if err != nil {
+		return err
+	}
+	target, err = e.vectorizer.Corpi(context.TODO(), targetStringArr)
+	if err != nil {
+		return err
+	}
+
+	sourceWords, _, err := e.vectorizer.NearestWordsByVector(context.TODO(), source, 50)
+	if err != nil {
+		return err
+	}
+
+	targetWords, _, err := e.vectorizer.NearestWordsByVector(context.TODO(), target, 50)
+	if err != nil {
+		return err
+	}
+
+	// Moving towards the target, furthers away from the target
+	lowestDistance := float32(9999.0)
+	// for i := len(targetWords)-1; i >= 0; i-- {
+	for i := range targetWords {
+		if string(targetWords[i][0]) != "$" {
+			targetWordVector, err := e.vectorizer.Corpi(context.TODO(), []string{string(targetWords[i])})
+			if err != nil {
+				return err
+			}
+			distToResult, _ := e.distancer(targetWordVector, source)
+			if distToResult < lowestDistance {
+				fmt.Println("concept:", targetWords[i], "distanceToQuery", distToResult, "distanceToParent:", "UNSET", "distanceToNext:", "UNSET")
+				lowestDistance = distToResult
+			}
+		}
+	}
+
+	// Moving away from the source as start point, towards the target
+	for i := len(sourceWords)-1; i >= 0; i-- {
+		if string(sourceWords[i][0]) != "$" {
+			sourceWordVector, err := e.vectorizer.Corpi(context.TODO(), []string{string(sourceWords[i])})
+			if err != nil {
+				return err
+			}
+			distToResult, _ := e.distancer(sourceWordVector, target)
+			if distToResult < lowestDistance {
+				fmt.Println("concept:", sourceWords[i], "distanceToQuery:", distToResult, "distanceToParent:", "UNSET", "distanceToNext:", "UNSET")
+				lowestDistance = distToResult
+			}
+		}
+	}
+
+	return nil
+}
 
 func containedIn(haystack []string, needle string) (int, bool) {
 	for i, word := range haystack {
