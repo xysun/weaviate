3b1acf6ee9db2b42030f75e2853529eed503e790
Dirk Kulawiak <dirk@semi.technology>
2022-08-16 14:14:51 +0200

    DO NOT MERGE: run bratches of different size

--- test/benchmark/run_performance_tracker.sh
+++ test/benchmark/run_performance_tracker.sh
@@ -3,4 +3,7 @@
 # change to script directory
 cd "${0%/*}" || exit
 
-go run . -name "SIFT" -numberEntries 100000 -fail "-1" -numBatches "1"
+for i in "1" "2" "5"
+do
+   go run . -name "SIFT" -numberEntries 100000 -fail "-1" -numBatches $i
+done
