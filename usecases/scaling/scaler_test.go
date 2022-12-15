d0a9dcf06da5d8953fe9409d7572b7683c88ec9f
Redouan El Rhazouani <81578195+redouan-rhazouani@users.noreply.github.com>
2022-12-15 13:30:30 +0100

    Scaler: unit test basic setup

--- usecases/scaling/scaler_test.go
+++ usecases/scaling/scaler_test.go
@@ -1,1 +1,34 @@
 package scaling
+
+import (
+	"context"
+	"testing"
+
+	"github.com/semi-technologies/weaviate/usecases/sharding"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestScalerScale(t *testing.T) {
+	ctx := context.Background()
+	t.Run("NoShardingState", func(t *testing.T) {
+		scaler := newFakeFactory(0, 1, 0).Scaler()
+		old := sharding.Config{Replicas: 1}
+		_, err := scaler.Scale(ctx, "C", old, old)
+		assert.NotNil(t, err)
+		assert.Contains(t, err.Error(), "no sharding state")
+	})
+	t.Run("SameReplicationFactor", func(t *testing.T) {
+		scaler := newFakeFactory(1, 2, 2).Scaler()
+		old := sharding.Config{Replicas: 2}
+		_, err := scaler.Scale(ctx, "C", old, old)
+		assert.Nil(t, err)
+	})
+	t.Run("ScaleInNotSupported", func(t *testing.T) {
+		scaler := newFakeFactory(1, 2, 2).Scaler()
+		old := sharding.Config{Replicas: 2}
+		new := sharding.Config{Replicas: 1}
+		_, err := scaler.Scale(ctx, "C", old, new)
+		assert.NotNil(t, err)
+		assert.Contains(t, err.Error(), "not supported")
+	})
+}
