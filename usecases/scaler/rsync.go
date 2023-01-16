b28cc7e200e2a99796e1031f35e76a2c5da632ea
Redouan El Rhazouani <81578195+redouan-rhazouani@users.noreply.github.com>
2023-01-16 19:43:17 +0100

    Scaler: add target node to returned error when syncing shards

--- usecases/scaler/rsync.go
+++ usecases/scaler/rsync.go
@@ -75,38 +75,38 @@ func (r *rsync) PushShard(ctx context.Context, className string, desc backup.Sha
 			return fmt.Errorf("%w: %q", ErrUnresolvedName, targetNode)
 		}
 		if err := r.client.CreateShard(ctx, host, className, desc.Name); err != nil {
-			return fmt.Errorf("create new shard on remote node: %w", err)
+			return fmt.Errorf("create new shard on remote node %q: %w", targetNode, err)
 		}
 
 		// Transfer each file that's part of the backup.
 		for _, file := range desc.Files {
 			err := r.PutFile(ctx, file, host, className, desc.Name)
 			if err != nil {
-				return fmt.Errorf("copy files to remote node: %w", err)
+				return fmt.Errorf("copy files to remote node: %q: %w", targetNode, err)
 			}
 		}
 
 		// Transfer shard metadata files
 		err := r.PutFile(ctx, desc.ShardVersionPath, host, className, desc.Name)
 		if err != nil {
-			return fmt.Errorf("copy shard version to remote node: %w", err)
+			return fmt.Errorf("copy shard version to remote node %q: %w", targetNode, err)
 		}
 
 		err = r.PutFile(ctx, desc.DocIDCounterPath, host, className, desc.Name)
 		if err != nil {
-			return fmt.Errorf("copy index counter to remote node: %w", err)
+			return fmt.Errorf("copy index counter to remote node %q: %w", targetNode, err)
 		}
 
 		err = r.PutFile(ctx, desc.PropLengthTrackerPath, host, className, desc.Name)
 		if err != nil {
-			return fmt.Errorf("copy prop length tracker to remote node: %w", err)
+			return fmt.Errorf("copy prop length tracker to remote node %q: %w", targetNode, err)
 		}
 
 		// Now that all files are on the remote node's new shard, the shard needs
 		// to be reinitialized. Otherwise, it would not recognize the files when
 		// serving traffic later.
 		if err := r.client.ReInitShard(ctx, host, className, desc.Name); err != nil {
-			return fmt.Errorf("create new shard on remote node: %w", err)
+			return fmt.Errorf("create new shard on remote node: %q: %w", targetNode, err)
 		}
 	}
 	return nil
